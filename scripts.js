99ea8a89bb35621cc184c639c58026758a58150e
TheRadioGalaxy <admin@theradiogalaxy.com>
2016-04-14 13:40:51 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   
   $('#content').load('home.html');
   
-  $('nav-bar-content ul li').hover(function(){
+  $('#nav-bar-content ul li').hover(function(){
     $(this).css('background-color', 'blue');
   }, function() {
     $(this).css('background-color', '');
