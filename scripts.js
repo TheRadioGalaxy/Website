667e8f9a38993b36359bfa0b6abb288575708acf
TheRadioGalaxy <admin@theradiogalaxy.com>
2016-04-18 17:34:59 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
   
   //$('#content').load('home.html');
   
+  $('.icons img').hover(function(){
+    $(this).css('width', '60px');
+    $(this).css('height', '60px');
+  }, function() {
+    $(this).css('width', '50px');
+    $(this).css('height', '50px');
+  })
+  
   $('#contactLink').click(function(){
     $('#content').load('contact.html');
   });
