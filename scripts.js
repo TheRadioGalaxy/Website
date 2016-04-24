d526fe32604fa88702a09bef4c52be075921e714
TheRadioGalaxy <admin@theradiogalaxy.com>
2016-04-24 13:05:37 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   
   //$('#content').load('home.html');
   
-  (function(){var ticker=document.createElement('script');
+  (function(){
+    var ticker=document.createElement('script');
     ticker.type='text/javascript';ticker.async=true;
     ticker.src='//twitcker.com/ticker/radiogalaxyblog.js';
     (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(ticker);
