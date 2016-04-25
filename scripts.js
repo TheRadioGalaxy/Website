143baa4c90ef8076abe212cb7208d61fd8722353
TheRadioGalaxy <admin@theradiogalaxy.com>
2016-04-24 21:17:41 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,6 +8,18 @@ $(document).ready(function() {
     ticker.src='//twitcker.com/ticker/radiogalaxyblog.js?background=000000&speed=6&count=50&tweet=0050ff&container=footer';
     (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(ticker);
   })();
+  
+  $('#content').socialist({
+    networks: [
+      {name:'linkedin',id:'buddy-media'},
+      {name:'facebook',id:'in1dotcom'},
+      {name:'pinterest',id:'potterybarn'},
+      {name:'twitter',id:'in1dotcom'},
+      {name:'googleplus',id:'105588557807820541973/posts'},
+      {name:'rss',id:' http://feeds.feedburner.com/good/lbvp'},
+      {name:'craigslist',id:'boo',areaName:'southcoast'}
+    ]
+  });
 
   
   $('#interviewsLink').click(function(){
