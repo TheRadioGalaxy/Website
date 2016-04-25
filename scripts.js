c3353a57f38b57e1681f729d65bf8e2b13eb6f1b
TheRadioGalaxy <admin@theradiogalaxy.com>
2016-04-25 09:53:18 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,21 +9,7 @@ $(document).ready(function() {
     (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(ticker);
   })();
   
-  $('.social-feed-container').socialfeed({
-            // INSTAGRAM
-            instagram:{
-                accounts: ['@teslamotors','#teslamotors'],  //Array: Specify a list of accounts from which to pull posts
-                limit: 2,                                    //Integer: max number of posts to load
-                client_id: 'YOUR_INSTAGRAM_CLIENT_ID'       //String: Instagram client id (optional if using access token)
-                access_token: 'YOUR_INSTAGRAM_ACCESS_TOKEN' //String: Instagram access token
-            },
-
-            // GENERAL SETTINGS
-            length:400                                      //Integer: For posts with text longer than this length, show an ellipsis.
-        });
-    });
-    
-    
+  
 $('.social-feed-container').socialfeed({
     // FACEBOOK
     facebook:{
